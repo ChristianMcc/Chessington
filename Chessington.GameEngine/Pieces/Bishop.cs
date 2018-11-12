e4a377a25b92e9628f4edf729874bce0bb53d7b4
Christian McCaffery <Christian.McCaffery@softwire.com>
2018-11-12 15:11:23 +0000

    Queen can move as rook and bishop can combined (diagonally and laterally)

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -15,11 +15,9 @@ namespace Chessington.GameEngine.Pieces
             for (var i = 0; i < 8; i++)
                 availableMoves.Add(Square.At(i, i));
 
-            //Checking the forwards diagonal i.e. 5,3 6,2 7,1
             for (var i = 1; i < 8; i++)
                 availableMoves.Add(Square.At(i, 8 - i));
 
-            //Get rid of our starting location.
             availableMoves.RemoveAll(s => s == Square.At(currentSquare.Row, currentSquare.Col));
 
             return availableMoves;
