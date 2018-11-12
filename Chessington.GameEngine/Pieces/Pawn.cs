0a2a03668097d6da67017659cde3aff9d5d9af12
Christian McCaffery <Christian.McCaffery@softwire.com>
2018-11-12 14:51:10 +0000

    If pawn hasn't moved can move 2 squares, otherwise only 1

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -12,13 +12,32 @@ namespace Chessington.GameEngine.Pieces
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             var availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
             if (Player == Player.White)
             {
-                availableMoves.Add(Square.At(6, 0));
+                if (currentSquare.Row == 7)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row - 2, currentSquare.Col));
+                    availableMoves.Add(Square.At(6, 0));
+                }
+                else
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row - 1, currentSquare.Col));
+                }
+                
             }
             if (Player == Player.Black)
             {
-                availableMoves.Add(Square.At(2, 0));
+                if (currentSquare.Row == 1)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row + 2, currentSquare.Col));
+                    availableMoves.Add(Square.At(2, 0));
+                }
+                else
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row + 1, currentSquare.Col));
+                }
+                
             }
 
             return availableMoves;
