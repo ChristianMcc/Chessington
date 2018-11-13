5e5680a2e28e5b94e8d18dae579c7d183aa3f098
Christian McCaffery <Christian.McCaffery@softwire.com>
2018-11-13 10:03:53 +0000

    Knights cannot take friendly pieces

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -12,6 +12,7 @@ namespace Chessington.GameEngine.Pieces
         {
             var availableMoves = new List<Square>();
             var currentSquare = board.FindPiece(this);
+            var player = Player;
 
             availableMoves.Add(Square.At(currentSquare.Row - 2, currentSquare.Col + 1));
             availableMoves.Add(Square.At(currentSquare.Row - 2, currentSquare.Col - 1));
@@ -27,6 +28,12 @@ namespace Chessington.GameEngine.Pieces
             foreach (var square in availableMoves)
             {
                 if (square.Row < 0 || square.Col < 0 || square.Row > 7 || square.Col > 7)
+                {
+                    movesToRemove.Add(square);
+                    continue;
+                }
+
+                if (board.GetPiece(square) != null && board.GetPiece(square).Player == board.CurrentPlayer)
                 {
                     movesToRemove.Add(square);
                 }
